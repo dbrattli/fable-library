161cc8aed9635772ddca3d71bad9868264b56d1c
Dag Brattli <dag@brattli.net>
2021-01-20 08:18:53 +0100

    Added actions

--- fable/list.py
+++ fable/list.py
@@ -1,19 +1,27 @@
 # flake8: noqa
 
-from expression.collections import frozenlist
+from typing import Any, Callable, TypeVar
+
+from expression.collections import FrozenList
+
+A = TypeVar("A")
+B = TypeVar("B")
 
 
-def empty():
-    return frozenlist.empty
+def empty() -> FrozenList[Any]:
+    return FrozenList.empty()
 
-def map(mapper, lst):
+
+def map(mapper: Callable[[A], B], lst: FrozenList[A]) -> FrozenList[B]:
     return lst.map(mapper)
 
-def filter(predicate, lst):
+
+def filter(predicate: Callable[[A], bool], lst: FrozenList[A]) -> FrozenList[A]:
     return lst.filter(predicate)
 
-ofArray = frozenlist.of_seq
-ofSeq = frozenlist.of_seq
-singleton = frozenlist.singleton
+
+ofArray = FrozenList.of_seq
+ofSeq = FrozenList.of_seq
+singleton = FrozenList.singleton
 
 __all__ = ["map", "ofArray", "ofSeq", "singleton"]
--- fable/seq.py
+++ fable/seq.py
@@ -1,11 +1,17 @@
-from expression.collections import seq
+from typing import Callable, TypeVar
+from expression.collections import Seq, seq
+
+A = TypeVar("A")
+B = TypeVar("B")
 
 
-def map(mapper, seq):
-    return seq.map(mapper)
+def map(mapper: Callable[[A], B], xs: Seq[A]) -> Seq[B]:
+    return xs.map(mapper)
 
 
+delay = seq.delay
 rangeNumber = seq.range
-delay = frozenlist.singleton
+singleton = seq.singleton
 
-__all__ = ["ofArray", "singleton"]
+
+__all__ = ["delay", "map", "rangeNumber", "singleton"]
